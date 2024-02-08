386e34c6897b8f32119e07d7c42a511cb897255d
123nush <anushkadal1611@gmail.com>
2024-02-08 10:02:55 +0530

    grid view

--- app/src/main/java/com/example/sanuanu/Other_layouts.java
+++ app/src/main/java/com/example/sanuanu/Other_layouts.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 public class Other_layouts extends AppCompatActivity {
@@ -17,5 +18,9 @@ public class Other_layouts extends AppCompatActivity {
         ListView list1=findViewById(R.id.list1);
         ArrayAdapter<String> arr=new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,name);
         list1.setAdapter(arr);
+        String sub[]={"AI","ML","AWP"};
+        ArrayAdapter<String> s=new ArrayAdapter<>(this, androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item,sub);
+        GridView g1=findViewById(R.id.grid1);
+        g1.setAdapter(s);
     }
 }
